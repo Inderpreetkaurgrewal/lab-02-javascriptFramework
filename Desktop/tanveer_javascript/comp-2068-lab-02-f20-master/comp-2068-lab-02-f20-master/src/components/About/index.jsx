bc12c48f372af2ff53b43b7d45a566262c86f961
inderpreetkaurgrewal <59539393+Inderpreetkaurgrewal@users.noreply.github.com>
2020-10-25 15:59:14 -0400

    init of react app

---
+++ Desktop/tanveer_javascript/comp-2068-lab-02-f20-master/comp-2068-lab-02-f20-master/src/components/About/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../shared/Header';
+
+const Home = () => {
+  return (
+    <>
+    <Header title = "Hi!  I am Tanveer Kaur. My favourite singer is sidhu moosewala . I love his all songs."/>
+      {/* You need to use your shared header component... */}
+      {/* You need a paragraph describing a fun fact about you */}
+    </>
+  );
+}
+ 
+export default Home;
---
+++ Desktop/tanveer_javascript/comp-2068-lab-02-f20-master/comp-2068-lab-02-f20-master/src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../shared/Header';
+// You need to import your shared header...
+
+const Home = () => {
+  return (
+    <>
+  <Header title ="Welcome Home"/>;
+  </>
+  );
+}
+ 
+export default Home;
