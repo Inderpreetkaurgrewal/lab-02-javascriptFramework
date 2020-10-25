029e4374a56eb087a7567597e6da3ea43a02d6ba
inderpreetkaurgrewal <59539393+Inderpreetkaurgrewal@users.noreply.github.com>
2020-10-25 10:56:58 -0400

    init of react app

---
+++ Desktop/JavascriptFrameworks/Lab-02/comp-2068-lab-02-f20-master/comp-2068-lab-02-f20-master/src/components/About/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../shared/Header';
+
+const Home = () => {
+  return (
+    <>
+    <Header title = "Hi! This is inderpreet kaur grewal. I love playing sports like badminton, cricket, volleyball.l. I enjoy listening to music, reading books and cooking. I like dancing."/>
+      {/* You need to use your shared header component... */}
+      {/* You need a paragraph describing a fun fact about you */}
+    </>
+  );
+}
+ 
+export default Home;
---
+++ Desktop/JavascriptFrameworks/Lab-02/comp-2068-lab-02-f20-master/comp-2068-lab-02-f20-master/src/components/Nav/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+// You're likely missing some imports...
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Styles from './styles';
+
+// Don't forget to import your components
+import Home from '../Home';
+import About from '../About';
+const Nav = () => {
+  return (
+    <Router>
+      <Styles.Nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+        </ul>
+      </Styles.Nav>
+
+      <Switch>
+        <Route exact path="/Home"><Home/></Route>
+        <Route exact path="/About"><About/></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Nav;
