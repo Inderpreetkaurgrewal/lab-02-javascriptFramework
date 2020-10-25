5f0df32001dcf6cbc28ae58d7ea9ff3d0571ae94
inderpreetkaurgrewal <59539393+Inderpreetkaurgrewal@users.noreply.github.com>
2020-10-25 16:11:03 -0400

    init of react app

--- Desktop/tanveer_javascript/comp-2068-lab-02-f20-master/comp-2068-lab-02-f20-master/src/components/About/index.jsx
+++ Desktop/tanveer_javascript/comp-2068-lab-02-f20-master/comp-2068-lab-02-f20-master/src/components/About/index.jsx
@@ -4,7 +4,7 @@ import Header from '../shared/Header';
 const Home = () => {
   return (
     <>
-    <Header title = "Hi!  I am Tanveer Kaur. My favourite singer is sidhu moosewala . I love his all songs."/>
+    <Header title = "Hi! Tanveer Kaur. My favourite singer is sidhu moosewala . I love his all songs."/>
       {/* You need to use your shared header component... */}
       {/* You need a paragraph describing a fun fact about you */}
     </>
